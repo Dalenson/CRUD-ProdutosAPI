dadc66de8a7af2fa3c096a709c1f3927f3e2fedf
Dalenson <dal.colares@hotmail.com>
2022-08-23 19:35:52 -0300

    Implementações

--- src/main/java/com/dale/projetoestudo/ProjetoEstudoApplication.java
+++ src/main/java/com/dale/projetoestudo/ProjetoEstudoApplication.java
@@ -2,8 +2,10 @@ package com.dale.projetoestudo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProjetoEstudoApplication {
 
 	public static void main(String[] args) {
